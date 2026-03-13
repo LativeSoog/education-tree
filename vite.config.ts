d162d84110b57b0532c2959a57e0b0f0d2c2335c
Vitaliy <gusevvnn@gmail.com>
2026-03-14 02:05:47 +0500

    chore: install base dependencies, updated vite and eslint configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  plugins: [svgr({ svgrOptions: { icon: true } }), react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @use "@/assets/styles/variables" as *;
+        @use "@/assets/styles/mixins" as *;
+        `,
+      },
+    },
+  },
+});
