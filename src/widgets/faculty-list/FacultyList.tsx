8f70509eedb6ed9d945ae8f1037095073afeecf1
Vitaliy <gusevvnn@gmail.com>
2026-04-02 00:30:20 +0500

    feat(faculty): added the widget faculty list section

---
+++ src/widgets/faculty-list/FacultyList.tsx
@@ -0,0 +1,9 @@
+import { FacultyItem } from "@/entities/faculty/ui/faculty-item/FacultyItem";
+
+export const FacultyList = () => {
+  return (
+    <section className="faculty-list">
+      <FacultyItem />
+    </section>
+  );
+};
