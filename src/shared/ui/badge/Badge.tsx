c5cc4448a939e3aacbc35672b1ab15eca12e7088
Vitaliy <gusevvnn@gmail.com>
2026-04-17 01:13:38 +0500

    feat(badge): added shared ui component badge

---
+++ src/shared/ui/badge/Badge.tsx
@@ -0,0 +1,16 @@
+import S from "./Badge.module.scss";
+import cn from "classnames";
+
+type BadgeProps = {
+  variant: "blue";
+  children: React.ReactNode;
+  className?: string;
+};
+
+export const Badge = ({ variant, children, className }: BadgeProps) => {
+  return (
+    <div className={cn(S["badge"], S[`badge_${variant}`], className)}>
+      {children}
+    </div>
+  );
+};
