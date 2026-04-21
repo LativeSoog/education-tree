709633dc9571f607aa035e23d344d1f46935e61b
Vitaliy <gusevvnn@gmail.com>
2026-04-22 00:11:46 +0500

    refactor(faculty): move badges to program, renamed component

--- src/entities/program/ui/badges/ProgramBadges.tsx
+++ src/entities/program/ui/badges/ProgramBadges.tsx
@@ -1,20 +1,20 @@
 import { Badge } from "@/shared/ui/badge/Badge";
-import S from "./FacultyBadges.module.scss";
+import S from "./ProgramBadges.module.scss";
 
 export const FacultyBadges = () => {
   return (
-    <div className={S["faculty-badges"]}>
+    <div className={S["program-badges"]}>
       <Badge
         variant="blue"
-        className={S["faculty-badges__spec"]}>
-        <span className={S["faculty-badges__spec-code"]}>44.03.01</span>
-        <span className={S["faculty-badges__spec-label"]}>
+        className={S["program-badges__spec"]}>
+        <span className={S["program-badges__spec-code"]}>44.03.01</span>
+        <span className={S["program-badges__spec-label"]}>
           Педагогическое образование (с двумя профилями подготовки)
         </span>
       </Badge>
       <Badge
         variant="teal"
-        className={S["faculty-badges__form"]}>
+        className={S["program-badges__form"]}>
         Очно-заочная
       </Badge>
     </div>
