fb3ef13d01170d49b4acf9544582087180a7e5ab
Vitaliy <gusevvnn@gmail.com>
2026-04-22 00:57:20 +0500

    feat(icon): added book icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,3 @@
 export { default as FacultyIcon } from "./ui/faculty.svg?react";
+export { default as BookIcon } from "./ui/book.svg?react";
 export { default as DoubleArrow } from "./arrows/double-arrow.svg?react";
