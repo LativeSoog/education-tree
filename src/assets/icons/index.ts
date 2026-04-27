85445613177d0a28238df057025a99ba6bbb1173
Vitaliy <gusevvnn@gmail.com>
2026-04-28 00:44:34 +0500

    feat(icon): added icon people

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,5 @@
 export { default as FacultyIcon } from "./ui/faculty.svg?react";
 export { default as BookIcon } from "./ui/book.svg?react";
+export { default as PeopleIcon } from "./ui/people.svg?react";
+
 export { default as DoubleArrow } from "./arrows/double-arrow.svg?react";
