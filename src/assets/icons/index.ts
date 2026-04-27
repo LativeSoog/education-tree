d117ef6ae7b6e4d0743904c073fc1eff941de042
Vitaliy <gusevvnn@gmail.com>
2026-04-28 01:20:57 +0500

    feat(icon): added exam icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,5 +1,6 @@
 export { default as FacultyIcon } from "./ui/faculty.svg?react";
 export { default as BookIcon } from "./ui/book.svg?react";
 export { default as PeopleIcon } from "./ui/people.svg?react";
+export { default as ExamIcon } from "./ui/exam.svg?react";
 
 export { default as DoubleArrow } from "./arrows/double-arrow.svg?react";
