c7722d4ec6fb5f9b4deeaccc8f3dca7d9ea2fad1
Vitaliy <gusevvnn@gmail.com>
2026-05-25 01:57:19 +0500

    refactor(exam-item): added variant styles

--- src/entities/program/ui/exams/exam-item/ExamItem.tsx
+++ src/entities/program/ui/exams/exam-item/ExamItem.tsx
@@ -1,18 +1,21 @@
 import S from "./ExamItem.module.scss";
+import cn from "classnames";
 
 type ExamItemProps = {
   priority: string;
   exams: string[];
   choice?: boolean;
+  variant: "ege" | "vi";
 };
 
 export const ExamItem = ({
   priority,
   exams,
   choice = false,
+  variant,
 }: ExamItemProps) => {
   return (
-    <div className={S["exam-item"]}>
+    <div className={cn(S["exam-item"], S[`exam-item_${variant}`])}>
       <div className={S["exam-item__priority"]}>{priority}</div>
       <div className={S["exam-item__list"]}>
         {exams.map((exam) => (
