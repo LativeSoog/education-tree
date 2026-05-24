16fe41fd72b80f2f5df87ebc3c81d950d6d16d8c
Vitaliy <gusevvnn@gmail.com>
2026-05-25 02:11:10 +0500

    feat(program-exams): added lists for ege and vi exams

--- src/entities/program/ui/exams/ProgramExams.tsx
+++ src/entities/program/ui/exams/ProgramExams.tsx
@@ -1,6 +1,6 @@
 import { ExamIcon } from "@/assets/icons";
 import S from "./ProgramExams.module.scss";
-import { ExamsEge } from "./exams-ege/ExamsEge";
+import { ExamsList } from "./exams-list/ExamsList";
 
 export const ProgramExams = () => {
   return (
@@ -12,7 +12,14 @@ export const ProgramExams = () => {
         <h4 className={S["program-exams__title"]}>Вступительные испытания</h4>
       </div>
       <div className={S["program-exams__main"]}>
-        <ExamsEge />
+        <ExamsList
+          label="Для поступающих на базе среднего общего образования (11 кл.) и высшего образования"
+          variant="ege"
+        />
+        <ExamsList
+          label="Для поступающих на базе среднего профессионального образования"
+          variant="vi"
+        />
       </div>
     </section>
   );
