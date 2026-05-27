bfc2b3a495fd11b0267505e81422ccbf10c8a6aa
Vitaliy <gusevvnn@gmail.com>
2026-05-28 02:39:04 +0500

    feat(programs-catalog): added function open modal calculator

--- src/widgets/programs-catalog/ui/catalog-toolbar/CatalogToolbar.tsx
+++ src/widgets/programs-catalog/ui/catalog-toolbar/CatalogToolbar.tsx
@@ -2,12 +2,17 @@ import { Button } from "@/shared/ui/button/Button";
 import S from "./CatalogToolbar.module.scss";
 import { CalculatorIcon } from "@/assets/icons";
 
-export const CatalogToolbar = () => {
+type CatalogToolbarProps = {
+  onCalcOpen: () => void;
+};
+
+export const CatalogToolbar = ({ onCalcOpen }: CatalogToolbarProps) => {
   return (
     <div className={S["catalog-toolbar"]}>
       <Button
         variant="primary"
-        className={S["catalog-toolbar__calc"]}>
+        className={S["catalog-toolbar__calc"]}
+        onClick={onCalcOpen}>
         <CalculatorIcon className={S["catalog-toolbar__calc-icon"]} />
         <span className={S["catalog-toolbar__calc-label"]}>
           Калькулятор ЕГЭ
