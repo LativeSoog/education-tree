f266a5bfd4b0ddf2c5efacf07cf464164d4bdd53
Vitaliy <gusevvnn@gmail.com>
2026-05-30 02:23:25 +0500

    chore: add icon trash

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,5 +3,6 @@ export { default as BookIcon } from "./ui/book.svg?react";
 export { default as PeopleIcon } from "./ui/people.svg?react";
 export { default as ExamIcon } from "./ui/exam.svg?react";
 export { default as CalculatorIcon } from "./ui/calculator.svg?react";
+export { default as TrashIcon } from "./ui/trash.svg?react";
 
 export { default as DoubleArrow } from "./arrows/double-arrow.svg?react";
