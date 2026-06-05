de9a56510ad4c9cba808a1050cd953a3ad727325
Vitaliy <gusevvnn@gmail.com>
2026-06-06 02:10:00 +0500

    refactor(main): update import function get programs

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { getProgramsData } from "./widgets/programs-catalog/model/getProgramsData.ts";
+import { getProgramsData } from "./widgets/programs-catalog/api/getProgramsData.ts";
 
 const PROGRAMS_DATA_MANIFEST_URL = import.meta.env.PROD
   ? "/wp-content/uploads/edu-tree/programs-catalog/manifest.json"
