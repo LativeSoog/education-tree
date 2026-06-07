5dd85fe310b23d367c86ef9fe18311e31ecc7497
Vitaliy <gusevvnn@gmail.com>
2026-06-08 01:08:19 +0500

    refactor(ui-icon): rename people icon to users rounded, add users icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,6 +1,7 @@
 export { default as FacultyIcon } from "./ui/faculty.svg?react";
 export { default as BookIcon } from "./ui/book.svg?react";
-export { default as PeopleIcon } from "./ui/people.svg?react";
+export { default as UsersRoundedIcon } from "./ui/users-rounded.svg?react";
+export { default as UsersIcon } from "./ui/users.svg?react";
 export { default as ExamIcon } from "./ui/exam.svg?react";
 export { default as CalculatorIcon } from "./ui/calculator.svg?react";
 export { default as TrashIcon } from "./ui/trash.svg?react";
