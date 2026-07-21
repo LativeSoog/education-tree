ce8c44d129c0a1c83dc77c3093d1232d3f2968e3
Vitaliy <gusevvnn@gmail.com>
2026-07-22 00:55:25 +0500

    refactor(badge): improve styles and update component

--- src/shared/ui/badge/Badge.tsx
+++ src/shared/ui/badge/Badge.tsx
@@ -2,14 +2,14 @@ import S from "./Badge.module.scss";
 import cn from "classnames";
 
 type BadgeProps = {
-  variant: "blue" | "teal";
+  tone: "blue" | "teal" | "red" | "gold" | "green";
   children: React.ReactNode;
   className?: string;
 };
 
-export const Badge = ({ variant, children, className }: BadgeProps) => {
+export const Badge = ({ tone, children, className }: BadgeProps) => {
   return (
-    <div className={cn(S["badge"], S[`badge_${variant}`], className)}>
+    <div className={cn(S["badge"], S[`badge_${tone}`], className)}>
       {children}
     </div>
   );
